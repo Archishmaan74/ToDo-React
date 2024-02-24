f8d497abd55ca107eb8500c436bca4c9cb3653e4
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2024-02-24 14:31:46 +0530

    created task component

--- frontend/src/components/Tasks.tsx
+++ frontend/src/components/Tasks.tsx
@@ -1,8 +1,47 @@
+import { useEffect, useState } from "react";
+import "../styles/task.css";
 
 function Tasks() {
+  const [data, setData] = useState([]);
+  const [fetched, setFetched] = useState(false);
+  useEffect(() => {
+    fetch("http://localhost:3000/display")
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json);
+        setFetched(true);
+      });
+  }, []);
   return (
-    <div>Tasks</div>
-  )
+    <>
+      <div className="center">
+        <h1>Todo List</h1>
+      </div>
+      <div className="tablestyle">
+        {fetched === true ?
+        <table className="table table-striped">
+        <thead className="center">
+          <tr>
+            <th>Sno</th>
+            <th>Task</th>
+            <th>Date</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+        <tbody className="center">
+          {data.map((d: any)=>(
+            <tr>
+              <td>{d.sno}</td>
+              <td>{d.task}</td>
+              <td>{d.date}</td>
+              <td>{d.time}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>: <div>Loading...</div>}
+      </div>
+    </>
+  );
 }
 
-export default Tasks
+export default Tasks;
