28d2f6c7abb3cfe3dbf766ef928ff8c5b5cb3b88
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2024-02-27 15:52:47 +0530

    created and routed add and edit task components

--- frontend/src/components/AddTask.tsx
+++ frontend/src/components/AddTask.tsx
@@ -1,7 +1,69 @@
+import axios from "axios";
+import "../styles/add.css";
+import { useState } from "react";
+
 
 function AddTask() {
+  const [sno, setSno] = useState("");
+  const [task, setTask] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");  
+  const data = {
+    sno: "",
+    task: "",
+    date: "",
+    time: ""
+  };
+  const [inputData, setInputData] = useState(data);
+
+  const handleData = (e: any) => {
+    setInputData({ ...inputData, [e.target.name]: e.target.value});
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    axios.post("http://127.0.0.1:3000/add", inputData).then((response) => {
+        console.log(response);
+        alert("Task Added successfully!");
+    })
+  }
   return (
-    <div>AddTask</div>
+    <>
+<div className="center">
+        <h1>Add Task</h1>
+      </div>
+      <div className="center">
+        Sno: <input type="number" name="sno" required value={inputData.sno} onChange={(e) =>  {
+            setSno(e.target.value);
+            handleData(e);
+        }}/>
+        <br />
+        <br />
+        Task: <input type="text" name="task" required value={inputData.task} onChange={(e) => {
+            setTask(e.target.value);
+            handleData(e);
+        }} />
+        <br />
+        <br />
+        Date: <input type="text" name="date" required value={inputData.date} onChange={(e) => {
+            setDate(e.target.value);
+            handleData(e);
+        }} />
+        <br />
+        <br />
+        Time: <input type="text" name="time" required value={inputData.time} onChange={(e) => {
+            setTime(e.target.value);
+            handleData(e);
+        }} />
+        <br />
+        <br />
+        {sno.length >0 && task.length >0 && date.length>0 && time.length>0 ? (
+            <button className="btn btn-primary" onClick={(e) =>{
+                handleSubmit(e);
+            }}>Add</button>
+        ) : (<p>Please fill all the above fields!</p>)}
+      </div>
+    </>
   )
 }
 
--- frontend/src/components/EditTask.tsx
+++ frontend/src/components/EditTask.tsx
@@ -1,9 +1,67 @@
+import { useState } from "react"
+import "../styles/edit.css"
+import axios from "axios";
 
 function EditTask() {
+
+  const [sno, setSno] = useState("");
+  const [task, setTask] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");  
+  const data = {
+    sno: "",
+    task: "",
+    date: "",
+    time: ""
+  };
+  const [inputData, setInputData] = useState(data);
+
+  const handleData = (e: any) => {
+    setInputData({ ...inputData, [e.target.name]: e.target.value});
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+    axios.post("http://127.0.0.1:3000/edit", inputData).then((response) => {
+        console.log(response);
+        alert("Edited task successfully!");
+    })
+  }
   return (
     <>
-      <div>
+      <div className="center">
         <h1>Edit Task</h1>
+      </div>
+      <div className="center">
+        Sno: <input type="number" name="sno" required value={inputData.sno} onChange={(e) =>  {
+            setSno(e.target.value);
+            handleData(e);
+        }}/>
+        <br />
+        <br />
+        Task: <input type="text" name="task" required value={inputData.task} onChange={(e) => {
+            setTask(e.target.value);
+            handleData(e);
+        }} />
+        <br />
+        <br />
+        Date: <input type="text" name="date" required value={inputData.date} onChange={(e) => {
+            setDate(e.target.value);
+            handleData(e);
+        }} />
+        <br />
+        <br />
+        Time: <input type="text" name="time" required value={inputData.time} onChange={(e) => {
+            setTime(e.target.value);
+            handleData(e);
+        }} />
+        <br />
+        <br />
+        {sno.length >0 && task.length >0 && date.length>0 && time.length>0 ? (
+            <button className="btn btn-primary" onClick={(e) =>{
+                handleSubmit(e);
+            }}>Edit</button>
+        ) : (<p>Please fill all the above fields!</p>)}
       </div>
     </>
   )
