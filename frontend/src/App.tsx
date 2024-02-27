16e2dbe37e01d0722400d2a7f87c3f685e28ceef
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2024-02-27 14:10:50 +0530

    impleted action buttons in tasks list and added edit route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Tasks from "./components/Tasks"
 import AddTask from "./components/AddTask"
+import EditTask from "./components/EditTask"
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Tasks/>} />
             <Route path="/addtask" element={<AddTask/>} />
+            <Route path="/edittask" element={<EditTask/>}/>
           </Routes>
         </div>
       </div>
