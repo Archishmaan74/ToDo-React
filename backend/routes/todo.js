414743c6b5ae9a06ecc6eed9df6c07e3c7257b16
Archishmaan Upadhyaya <archishmaan.upa4@gmail.com>
2024-02-27 15:51:36 +0530

    implemented addtask API

--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -15,6 +15,22 @@ router.get("/display", async(req,res)=>{
     res.send(await TaskModel.find().sort({sno:1}));
 })
 
+router.post("/add",async(req,res)=>{
+    const { sno, task, date, time } = req.body;
 
+  try{
+    const newTask = new TaskModel({
+        sno,
+        task,
+        date,
+        time
+    });
+    const savedTask = await newTask.save();
+
+    res.status(201).json(savedTask);
+  }catch(err){
+    res.status(400).json({ message: err.message });
+  }
+})
 
 module.exports = router;
